0fbd847f0036ac2d14c2234f78b426f04f3a91da
Tô Phạm Thành Đạt <todat2207@gmail.com>
2025-11-02 16:14:17 +0700

    new smc

--- contracts/ViePropChainNFT.sol
+++ contracts/ViePropChainNFT.sol
@@ -8,17 +8,45 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract ViePropChainNFT is ERC721URIStorage, ERC721Enumerable, Ownable {
     uint256 public tokenCounter;
+    
+    // Mapping to check if a tokenURI already exists
+    mapping(string => bool) private _tokenURIExists;
+    mapping(string => uint256) private _tokenURIToTokenId;
+    
+    // Events
+    event NFTMinted(uint256 indexed tokenId, address indexed to, string tokenURI);
+    event DuplicateMintAttempted(string tokenURI, uint256 existingTokenId);
 
     constructor() ERC721("ViePropChain", "VPC") Ownable(msg.sender) {}
 
     function mint(
         address recipient,
-        string memory tokenURI
+        string memory _tokenURI
     ) public onlyOwner returns (uint256) {
+        // Check if tokenURI already exists
+        require(!_tokenURIExists[_tokenURI], "NFT with this metadata already exists");
+        
         tokenCounter++;
         _mint(recipient, tokenCounter);
-        _setTokenURI(tokenCounter, tokenURI);
+        _setTokenURI(tokenCounter, _tokenURI);
+        
+        // Mark tokenURI as used
+        _tokenURIExists[_tokenURI] = true;
+        _tokenURIToTokenId[_tokenURI] = tokenCounter;
+        
+        emit NFTMinted(tokenCounter, recipient, _tokenURI);
         return tokenCounter;
+    }
+    
+    // Function to check if a tokenURI already exists
+    function tokenURIExists(string memory _tokenURI) public view returns (bool) {
+        return _tokenURIExists[_tokenURI];
+    }
+    
+    // Function to get tokenId by tokenURI
+    function getTokenIdByURI(string memory _tokenURI) public view returns (uint256) {
+        require(_tokenURIExists[_tokenURI], "TokenURI does not exist");
+        return _tokenURIToTokenId[_tokenURI];
     }
 
     function _update(address to, uint256 tokenId, address auth)
